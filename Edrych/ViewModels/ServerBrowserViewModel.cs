3de922852a6a70ca48ebceb6544930c448286570
hewitte <hewittej+github@gmail.com>
2011-11-10 10:30:47 -0600

    Refactored so all the work was done on the ViewModel, and the Model is just a structure

--- Edrych/ViewModels/ServerBrowserViewModel.cs
+++ Edrych/ViewModels/ServerBrowserViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Aga.Controls.Tree;
 using Edrych.DataAccess;
 using Edrych.Dialogs;
@@ -20,7 +22,7 @@ namespace Edrych.ViewModels
 
         public ServerBrowserViewModel()
         {
-            _tree = new ServerBrowserModel();
+            _tree = new ServerBrowserModel(this);
             CreateConnection();
         }
 
@@ -40,7 +42,7 @@ namespace Edrych.ViewModels
 
         #endregion
 
-        #region Public Methods
+        #region Public Methods - Called from View
 
         public void CreateConnection()
         {
@@ -48,19 +50,65 @@ namespace Edrych.ViewModels
             cd.ShowDialog();
             if (cd.DataAccess != null)
             {
-                _tree.AddServer(cd.DataAccess);
+                this.AddServer(cd.DataAccess);
                 this._activeConnection = cd.DataAccess;
             }
         }
 
-        public void RefreshNode(TreeNodeAdv Node)
+        public void RefreshNode(TreeNodeAdv SelectedNode)
         {
-            _tree.RefreshNode(Node);
+            if (SelectedNode != null)
+            {
+                BaseItem item = SelectedNode.Tag as BaseItem;
+                List<BaseItem> items = new List<BaseItem>();
+                items.Add(item);
+                BaseItem parent = item.Parent;
+                while (parent != null)
+                {
+                    items.Insert(0, parent);
+                    parent = parent.Parent;
+                }
+
+                TreePath path = new TreePath(items.ToArray());
+                RemoveCachedItems(_tree.Cache.Keys.Where(k => k.StartsWith(item.ItemPath)).ToList());
+
+                _tree.OnStructureChanged(path);
+            }
         }
 
-        public void RemoveConnection(TreeNodeAdv Node)
+        public void RemoveConnection(TreeNodeAdv SelectedNode)
         {
-            _tree.RemoveServer(Node);
+            //Find the server
+            ServerItem server = null;
+            while (server == null)
+            {
+                if (SelectedNode == null)
+                {
+                    break;
+                }
+                BaseItem item = SelectedNode.Tag as BaseItem;
+
+                if (item.Type == ItemType.Server)
+                {
+                    server = SelectedNode.Tag as ServerItem;
+                    break;
+                }
+
+                SelectedNode = SelectedNode.Parent;
+            }
+
+            if (server != null)
+            {
+                //Next, clear the cache
+                int index = SelectedNode.Index;
+                _tree.Cache["ROOT"].Remove(server as BaseItem);
+                List<BaseItem> serverChildren = _tree.Cache.ContainsKey(server.ItemPath) ? serverChildren = _tree.Cache[server.ItemPath] : new List<BaseItem>();
+                RemoveCachedItems(_tree.Cache.Keys.Where(k => k.StartsWith(server.ItemPath)).ToList());
+                server.Dispose();
+
+                //Finally, fire the event off
+                _tree.OnNodesRemoved(TreePath.Empty, new int[] { index }, new object[] { server });
+            }
         }
 
         public void UpdateActiveConnection(TreeNodeAdv Node)
@@ -92,7 +140,185 @@ namespace Edrych.ViewModels
 
         #endregion
 
+        #region Public Methods - Called from Model
+
+        public IEnumerable GetChildren(TreePath treePath)
+        {
+            List<BaseItem> items = new List<BaseItem>();
+            if (treePath.IsEmpty())
+            {
+                if (_tree.Cache.ContainsKey("ROOT"))
+                {
+                    items = _tree.Cache["ROOT"];
+                }
+                else
+                {
+                    items = new List<BaseItem>();
+                    _tree.Cache.Add("ROOT", items);
+                }
+            }
+            else
+            {
+                ServerItem server = treePath.FirstNode as ServerItem;
+                BaseItem database = treePath.FullPath.FirstOrDefault(i => (i as BaseItem).Type == ItemType.Database) as BaseItem;
+                BaseItem parent = treePath.LastNode as BaseItem;
+                if (parent != null && server != null)
+                {
+                    if (_tree.Cache.ContainsKey(parent.ItemPath))
+                    {
+                        items = _tree.Cache[parent.ItemPath];
+                    }
+                    else
+                    {
+                        //Populate the items based on the parent
+                        switch (parent.Type)
+                        {
+                            case ItemType.Server:
+                                items = GetDatabases(server);
+                                break;
+                            case ItemType.Database:
+                                items = GetDatabaseChildren(parent);
+                                break;
+                            case ItemType.Folder:
+                                if (database != null)
+                                    items = GetFolderChildren(parent, server, database.Name);
+                                break;
+                            case ItemType.Table:
+                            case ItemType.View:
+                                if (database != null)
+                                    items = GetColumns(parent, server, database.Name);
+                                break;
+                            default:
+                                break;
+                        }
+
+                        _tree.Cache.Add(parent.ItemPath, items);
+                    }
+
+                    parent.IsLoaded = true;
+                }
+            }
+
+            return items;
+        }
+
+        public bool IsLeaf(TreePath treePath)
+        {
+            return treePath.LastNode != null && ((BaseItem)treePath.LastNode).Type == ItemType.Column;
+        }
+
+        #endregion
+
+        #region Private Methods - Get Children
+
+        private List<BaseItem> GetDatabases(ServerItem server)
+        {
+            List<BaseItem> items = new List<BaseItem>();
+            List<Database> databases = server.DataAccess.GetDatabases();
+
+            foreach (Database db in databases)
+            {
+                BaseItem dataItem = new BaseItem(ItemType.Database, db.Name, server);
+                items.Add(dataItem);
+            }
+
+            return items;
+        }
+
+        private List<BaseItem> GetDatabaseChildren(BaseItem parent)
+        {
+            List<BaseItem> items = new List<BaseItem>();
+
+            BaseItem tables = new BaseItem(ItemType.Folder, "Tables", parent);
+            BaseItem views = new BaseItem(ItemType.Folder, "Views", parent);
+
+            items.Add(tables);
+            items.Add(views);
+
+            return items;
+        }
+
+        private List<BaseItem> GetFolderChildren(BaseItem parent, ServerItem server, string DatabaseName)
+        {
+            List<BaseItem> items = new List<BaseItem>();
+            ItemType type = ItemType.Table;
+            List<TableView> tableViews = null;
+
+            string oldDatabase = server.DataAccess.SelectedDatabase;
+            server.DataAccess.SetDatabase(DatabaseName);
+            if (parent.Name == "Tables")
+            {
+                tableViews = server.DataAccess.GetTables();
+                type = ItemType.Table;
+            }
+            else if (parent.Name == "Views")
+            {
+                tableViews = server.DataAccess.GetViews();
+                type = ItemType.View;
+            }
+            server.DataAccess.SetDatabase(oldDatabase);
+
+            if (tableViews != null)
+            {
+                foreach (TableView table in tableViews)
+                {
+                    items.Add(new BaseItem(type, table.Name, parent));
+                }
+            }
+
+            return items;
+        }
+
+        private List<BaseItem> GetColumns(BaseItem parent, ServerItem server, string DatabaseName)
+        {
+            List<BaseItem> items = new List<BaseItem>();
+            string oldDatabase = server.DataAccess.SelectedDatabase;
+            server.DataAccess.SetDatabase(DatabaseName);
+            foreach (Column col in server.DataAccess.GetColumns(parent.Name))
+            {
+                items.Add(new BaseItem(ItemType.Column, col.Name + " (" + col.DataType + ", " + (col.IsNullable ? "null" : "not null") + ")", parent));
+            }
+            server.DataAccess.SetDatabase(oldDatabase);
+
+            return items;
+        }
+
+        #endregion
+
         #region Private Methods
+
+        private void AddServer(DataAccessBase dataAccess)
+        {
+            ServerItem item = new ServerItem(dataAccess.DataSource);
+            item.DataAccess = dataAccess;
+            if (_tree.Cache.ContainsKey("ROOT"))
+            {
+                _tree.Cache["ROOT"].Add(item);
+            }
+            else
+            {
+                List<BaseItem> items = new List<BaseItem>();
+                items.Add(item);
+                _tree.Cache.Add("ROOT", items);
+            }
+
+            _tree.OnNodesInserted(TreePath.Empty, new int[] { _tree.Cache["ROOT"].Count - 1 }, new object[] { item });
+        }
+
+        private void RemoveCachedItems(List<string> oldCache)
+        {
+            foreach (string key in oldCache)
+            {
+                foreach (BaseItem oldItem in _tree.Cache[key])
+                {
+                    if (oldItem.Type == ItemType.Server)
+                        ((ServerItem)oldItem).Dispose();
+                    else
+                        oldItem.Dispose();
+                }
+                _tree.Cache.Remove(key);
+            }
+        }
 
         private TreePath GetNodePath(TreeNodeAdv Node)
         {
